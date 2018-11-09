ecd4cb4badfdc1f482046384ec8639cea4519c5a
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2018-11-09 19:00:54 -0200

    variables

---
+++ 2- Terraform Basic/1- variables/variables.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#testing what infrastructure will be changes
+terraform plan
+
+#apply changes
+terraform apply
+
+#destrpy changes
+terraform destroy
---
+++ 2- Terraform Basic/1- variables/vars.tf
@@ -0,0 +1,13 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-0f9351b59be17920e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-0d729a60"
+  }
+}
