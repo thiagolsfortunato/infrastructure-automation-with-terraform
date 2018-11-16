8cd226f445615642965c7e129b900c62a30ae740
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2018-11-16 16:07:19 -0200

    commands

---
+++ 2- Terraform Basic/7- commands/commands.sh
@@ -0,0 +1,25 @@
+Commands:
+    
+    apply              Builds or changes infrastructure
+    console            Interactive console for Terraform interpolations
+    destroy            Destroy Terraform-managed infrastructure
+    env                Workspace management
+    fmt                Rewrites config files to canonical format
+    get                Download and install modules for the configuration
+    graph              Create a visual graph of Terraform resources
+    import             Import existing infrastructure into Terraform
+    init               Initialize a Terraform working directory
+    output             Read an output from a state file
+    plan               Generate and show an execution plan
+    providers          Prints a tree of the providers used in the configuration
+    push               Upload this Terraform module to Atlas to run
+    refresh            Update local state file against real resources
+    show               Inspect Terraform state or plan
+    taint              Manually mark a resource for recreation
+    untaint            Manually unmark a resource as tainted
+    validate           Validates the Terraform files
+    version            Prints the Terraform version
+    workspace          Workspace management
+
+sudo apt install graphviz
+terraform graph | dot -Tsvg > graph.svg
